293866676164a767fb8f6706892b890de4082625
Bita Sobhani <bitasobhani@Bitas-MacBook-Pro.local>
2017-11-20 12:05:54 +0100

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Bita Sobhani on 20.11.17.
+//  Copyright © 2017 Bita Sobhani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Bita Sobhani on 20.11.17.
+//  Copyright © 2017 Bita Sobhani. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
