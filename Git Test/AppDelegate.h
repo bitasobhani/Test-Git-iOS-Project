1f21b1a49fda64dba0feea07fa5b600262785f38
Bita Sobhani <bitasobhani@Bitas-MacBook-Pro.local>
2017-11-20 11:34:46 +0100

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Bita Sobhani on 20.11.17.
+//  Copyright © 2017 Bita Sobhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Bita Sobhani on 20.11.17.
+//  Copyright © 2017 Bita Sobhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
